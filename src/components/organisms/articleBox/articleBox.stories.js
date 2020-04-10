d38e3f7a6955d37165a535cdfabf1e8e2e034760
Tan Keller <ersoichso@gmail.com>
2020-04-10 17:40:39 +0200

    Add ids to articles

--- src/components/organisms/articleBox/articleBox.stories.js
+++ src/components/organisms/articleBox/articleBox.stories.js
@@ -3,42 +3,59 @@ import React from 'react';
 import ArticleBox from './ArticleBox';
 
 export default {
-    component: ArticleBox,
-    title: 'Organism/ArticleBox'
-}
+  component: ArticleBox,
+  title: 'Organisms/ArticleBox'
+};
 
 const article = {
-    name: 'Summer Jacket Performance',
-    price: 75.90,
-    img: 'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
-}
+  id: 1,
+  name: 'Summer Jacket Performance',
+  price: 75.9,
+  img:
+    'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
+};
 
-export const Default = () => <ArticleBox style={{'max-width': '25%'}} article={article} />
-
+export const Default = () => (
+  <ArticleBox style={{ 'max-width': '25%' }} article={article} />
+);
 
 const articleWithLongTitle = {
-    name: 'Summer Jacket Performance with Pockets and a Weatherprotective',
-    price: 75.90,
-    img: 'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
-}
+  id: 1,
+  name: 'Summer Jacket Performance with Pockets and a Weatherprotective',
+  price: 75.9,
+  img:
+    'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
+};
 
-export const LongTitle = () => <ArticleBox style={{'max-width': '25%'}} article={articleWithLongTitle} />
-
+export const LongTitle = () => (
+  <ArticleBox style={{ 'max-width': '25%' }} article={articleWithLongTitle} />
+);
 
 const articleWithPseudoPrice = {
-    name: 'Summer Jacket Performance',
-    price: 75.90,
-    oldPrice: 99,
-    img: 'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
-}
+  id: 1,
+  name: 'Summer Jacket Performance',
+  price: 75.9,
+  oldPrice: 99,
+  img:
+    'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
+};
 
-export const ReducedArticle = () => <ArticleBox style={{'max-width': '25%'}} article={articleWithPseudoPrice} />
+export const ReducedArticle = () => (
+  <ArticleBox style={{ 'max-width': '25%' }} article={articleWithPseudoPrice} />
+);
 
 const articleWithLongTitlePseudoPrice = {
-    name: 'Summer Jacket Performance with Pockets and a Weatherprotective',
-    price: 75.90,
-    oldPrice: 99,
-    img: 'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
-}
+  id: 1,
+  name: 'Summer Jacket Performance with Pockets and a Weatherprotective',
+  price: 75.9,
+  oldPrice: 99,
+  img:
+    'https://demo.storefrontcloud.io/img/310/300/resize/m/j/mj11-black_main.jpg'
+};
 
-export const LongTitleReducedArticle = () => <ArticleBox style={{'max-width': '25%'}} article={articleWithLongTitlePseudoPrice} />
+export const LongTitleReducedArticle = () => (
+  <ArticleBox
+    style={{ 'max-width': '25%' }}
+    article={articleWithLongTitlePseudoPrice}
+  />
+);
