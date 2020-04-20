6b6f14f05bcc40c57c8282787660c24921f9e3c8
Tan Keller <ersoichso@gmail.com>
2020-04-20 11:16:32 +0200

    Add search input component

---
+++ src/components/atoms/searchBar/searchInput.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import SearchInput from './SearchInput';
+
+export default {
+  component: SearchInput,
+  title: 'Atoms/SearchInput',
+};
+
+export const Default = () => <SearchInput />;
