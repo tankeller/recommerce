8335f576c4d37c20010d9089dabf9170db7b50f5
Tan Keller <t.keller@reply.de>
2020-04-21 16:10:30 +0200

    Add proper template to listing

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -2,11 +2,14 @@
 import { jsx } from '@emotion/core';
 
 import PageTemplate from '../components/templates/PageTemplate';
+import ArticleList from '../components/organisms/articleList/ArticleList';
+
+import articles from '../assets/static/articles.json';
 
 const Listing = () => {
   return (
     <PageTemplate>
-      <div>This is the Listing Page</div>
+      <ArticleList articles={articles} />
     </PageTemplate>
   );
 };
