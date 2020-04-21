fb2ace475b245b8aaf82862ca778cbf75a11aa04
Tan Keller <t.keller@reply.de>
2020-04-21 13:00:17 +0200

    Add Footer component

---
+++ src/components/organisms/appFooter/AppFooter.js
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+const AppFooter = () => {
+  return (
+    <footer
+      css={{
+        width: '100%',
+        minHeight: '100px',
+        textAlign: 'center',
+      }}
+    >
+      ©copyrights by Portaltech
+    </footer>
+  );
+};
+
+export default AppFooter;
