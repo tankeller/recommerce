eae2ab9c4824cac94b9293f8911e3817864ac987
Tan Keller <t.keller@reply.de>
2020-04-21 16:41:32 +0200

    Add styling to header

--- src/components/organisms/appHeader/AppHeader.js
+++ src/components/organisms/appHeader/AppHeader.js
@@ -5,7 +5,14 @@ import MainHeaderNavigation from '../mainNavigation/MainHeaderNavigation';
 
 const AppHeader = () => {
   return (
-    <header>
+    <header
+      css={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '1.5rem calc((100vw - 1400px) / 2)',
+        borderBottom: '1px solid #eee',
+      }}
+    >
       <MainHeaderNavigation />
     </header>
   );
