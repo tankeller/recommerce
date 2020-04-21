c33e3b9e20c4c1cb7a06dc8d24d298c96894a1cf
Tan Keller <t.keller@reply.de>
2020-04-21 14:33:36 +0200

    Add Routing and pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from '@reach/router';
+
+import AppHeader from './components/organisms/appHeader/AppHeader';
+import AppFooter from './components/organisms/appFooter/AppFooter';
+import Home from './pages/Home';
+import Listing from './pages/Listing';
+import Article from './pages/Article';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <Router>
+        <Home path="/" />
+        <Listing path="/listing/:categoryID" />
+        <Article path="/article/:articleID" />
+      </Router>
+      <AppFooter />
     </div>
   );
 }
---
+++ src/pages/Listing.js
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
+const Listing = () => {
+  return <div>This is the Listing Page, yay</div>;
+};
+
+export default Listing;
