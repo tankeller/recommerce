33967a98fbf29b747334e57135de8f2d88089d87
Tan Keller <t.keller@reply.de>
2020-04-22 15:46:50 +0200

    Add more stories to ArticleData

--- src/components/molecules/articleData/articleData.stories.js
+++ src/components/molecules/articleData/articleData.stories.js
@@ -3,13 +3,21 @@ import React from 'react';
 import ArticleData from './ArticleData';
 
 export default {
-    component: ArticleData,
-    title: 'Molecules/ArticleData'
-}
+  component: ArticleData,
+  title: 'Molecules/ArticleData',
+};
 
 const article = {
-    name: 'Summer Jacket Performance with a Print',
-    price: 75.90
-}
+  name: 'Summer Jacket Performance with a Print',
+  price: 75.9,
+};
 
-export const Default = () => <ArticleData article={article}></ArticleData>
+export const Default = () => <ArticleData article={article} />;
+
+const reducedArticle = {
+  name: 'Summer Jacket Performance with a Print',
+  price: 60,
+  listPrice: 75.9,
+};
+
+export const ReducedArticle = () => <ArticleData article={reducedArticle} />;
