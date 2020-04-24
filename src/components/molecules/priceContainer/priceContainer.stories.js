3b535e6da6aa5032c5245bd0e6fe119cd1bc2cf7
Tan Keller <t.keller@reply.de>
2020-04-24 15:44:07 +0200

    Rename _id to id

--- src/components/molecules/priceContainer/priceContainer.stories.js
+++ src/components/molecules/priceContainer/priceContainer.stories.js
@@ -8,7 +8,7 @@ export default {
 };
 
 const article = {
-  _id: 86506,
+  id: 86506,
   img: 'https://www.fillmurray.com/250/250',
   name:
     'Irure ea sit ea ullamco sit deserunt in elit ut ullamco esse culpa elit',
@@ -20,7 +20,7 @@ const article = {
 export const Default = () => <PriceContianer article={article} />;
 
 const reducedArticle = {
-  _id: 413505,
+  id: 413505,
   img: 'https://www.fillmurray.com/250/250',
   name:
     'Aliquip labore incididunt fugiat eu non aliqua incididunt officia culpa consectetur officia sit',
