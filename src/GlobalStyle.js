df11032be9be5b745b626dd7766e59d19e461ca2
Tan Keller <t.keller@reply.de>
2020-04-24 15:53:12 +0200

    Remove broken css

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -9,8 +9,6 @@ const GlobalStyle = () => {
           margin: 0,
           fontFamily:
             '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans","Helvetica Neue", sans-serif',
-          '-webkit-font-smoothing': 'antialiased',
-          '-moz-osx-font-smoothing': 'grayscale',
         },
         code: {
           fontFamily:
