7e10c2c97d4464fcb5630d60e263ffcbce3c67b8
Tan Keller <t.keller@reply.de>
2020-04-28 10:47:02 +0200

    Add theme provider to access theme

--- src/components/atoms/categoryHeadline/categoryHeadline.stories.js
+++ src/components/atoms/categoryHeadline/categoryHeadline.stories.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'emotion-theming';
+
+import theme from '../../../assets/theme';
 
 import CategoryHeadline from './CategoryHeadline';
 
@@ -10,5 +13,7 @@ export default {
 };
 
 export const Default = () => (
-  <CategoryHeadline>{text('Categoryname', 'Furniture')}</CategoryHeadline>
+  <ThemeProvider theme={theme}>
+    <CategoryHeadline>{text('Categoryname', 'Furniture')}</CategoryHeadline>
+  </ThemeProvider>
 );
