bfd2face4849f865d6cedfeeeaad5debee681d39
Tan Keller <t.keller@reply.de>
2020-04-29 11:18:40 +0200

    Add font sizes to theme

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -4,6 +4,13 @@ const theme = {
     secondaryColor: 'red',
     textColor: '#000',
   },
+  font: {
+    sizes: {
+      small: '12px',
+      medium: '14px',
+      large: '16px',
+    },
+  },
 };
 
 export default theme;
