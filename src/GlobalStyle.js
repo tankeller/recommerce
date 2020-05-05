b0f5d03c11f2424d6b26c659eaf2efbadb2c470c
Mario Meissner <m.meissner@reply.de>
2020-05-05 19:32:41 +0200

    Add css reset to globalStyle component

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,21 +1,113 @@
 import React from 'react';
-import { Global } from '@emotion/core';
+import { Global, css } from '@emotion/core';
 
 const GlobalStyle = () => {
   return (
-    <Global
-      styles={{
-        body: {
-          margin: 0,
-          fontFamily:
-            '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans","Helvetica Neue", sans-serif',
-        },
-        'h1,h2,h3,h4,h5': {
-          fontFamily:
-            '"Roboto", "Oxygen", "Ubuntu", "Cantarell", "Droid Sans","Helvetica Neue", sans-serif',
-        },
-      }}
-    />
+    <>
+      <Global
+        styles={css`
+          /**
+           * ============================== RESETS ==============================
+           */
+
+          /* Box sizing rules */
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+
+          /* Remove default padding: */
+          ul,
+          ol {
+            padding: 0;
+          }
+
+          /* Remove default margin: */
+          body,
+          h1,
+          h2,
+          h3,
+          h4,
+          p,
+          ul,
+          ol,
+          figure,
+          blockquote,
+          dl,
+          dd {
+            margin: 0;
+          }
+
+          /* Set core body defaults */
+          body {
+            min-height: 100vh;
+            scroll-behavior: smooth;
+            text-rendering: optimizeSpeed;
+            line-height: 1.5;
+          }
+
+          /* Remove list styles on ul, ol elements with a class attribute */
+          ul[class],
+          ol[class] {
+            list-style: none;
+          }
+
+          /* A elements that don't have a class get default styles */
+          a:not([class]) {
+            text-decoration-skip-ink: auto;
+          }
+
+          /* Make images easier to work with */
+          img {
+            max-width: 100%;
+            display: block;
+          }
+
+          /* Natural flow and rhythm in articles by default */
+          article > * + * {
+            margin-top: 1em;
+          }
+
+          /* Inherit fonts for inputs and buttons */
+          input,
+          button,
+          textarea,
+          select {
+            font: inherit;
+          }
+
+          /* Make sure to set some focus styles for accessibility */
+          :focus {
+            outline: 0;
+          }
+
+          /* Remove all animations and transitions for people that prefer not to see them */
+          @media (prefers-reduced-motion: reduce) {
+            * {
+              animation-duration: 0.01ms !important;
+              animation-iteration-count: 1 !important;
+              transition-duration: 0.01ms !important;
+              scroll-behavior: auto !important;
+            }
+          }
+        `}
+      />
+
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+            fontFamily:
+              '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans","Helvetica Neue", sans-serif',
+          },
+          'h1,h2,h3,h4,h5': {
+            fontFamily:
+              '"Roboto", "Oxygen", "Ubuntu", "Cantarell", "Droid Sans","Helvetica Neue", sans-serif',
+          },
+        }}
+      />
+    </>
   );
 };
 
