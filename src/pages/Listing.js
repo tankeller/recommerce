c3816b81150c6541e38cbcb3eae2c4de9f0f352f
Mario Meissner <m.meissner@reply.de>
2020-05-14 20:39:37 +0200

    Change dummy data for categories and articles and filter articles data to the matching shop category

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -6,21 +6,28 @@ import ArticleList from '../components/organisms/articleList/ArticleList';
 import CategoryHeader from '../components/organisms/categoryHeader/CategoryHeader';
 
 import articles from '../assets/static/articles.json';
-
-const category = {
-  id: 115,
-  picture: 'https://www.fillmurray.com/640/360',
-  name: 'nulla non',
-  seotext:
-    'Nisi dolore tempor nulla in exercitation do officia dolor culpa non culpa laboris. Veniam ex laboris in ex aute ad ullamco aliquip incididunt enim non esse anim dolore. Sit cillum eu ea in labore qui consectetur aute. Cillum mollit amet occaecat incididunt culpa laboris pariatur excepteur mollit eiusmod veniam pariatur sint. Qui laborum labore et nostrud. Est adipisicing est magna adipisicing elit pariatur duis voluptate eu irure cillum. Adipisicing laborum tempor aliquip sit cupidatat minim dolor est duis velit esse elit.\r\nTempor eu officia tempor minim qui veniam incididunt dolore incididunt veniam duis et. Aliquip sunt est fugiat consequat fugiat. Qui laboris id ullamco amet aliquip irure dolore. Duis cupidatat elit anim dolore reprehenderit.\r\nPariatur reprehenderit occaecat nostrud eu dolore in consequat fugiat ex ad. Sunt tempor cillum excepteur reprehenderit esse quis Lorem mollit sit. Adipisicing est sit pariatur consectetur commodo reprehenderit qui. Labore sunt dolore sint ullamco magna id laborum cillum laboris excepteur id. Ipsum elit et non duis excepteur do consequat in.\r\nCupidatat adipisicing aliqua sunt laborum culpa ad qui sint aute cupidatat et non. Ut et exercitation tempor sint. Reprehenderit est aute ea mollit et veniam reprehenderit enim voluptate. Sint sit est eiusmod eiusmod.\r\nIncididunt minim et sit exercitation laboris aute esse mollit nostrud esse adipisicing excepteur tempor sint. Minim est tempor do minim in laboris est. Nostrud minim laborum magna irure occaecat sunt irure laboris esse velit.\r\n',
-};
+import categories from '../assets/static/categories.json';
 
 const Listing = ({ categoryID }) => {
+  // Articles by categoryID
+  const categoryArticles = articles.filter((article) => {
+    return article.categoryID === parseInt(categoryID);
+  });
+
+  // Articles Fallback: All articles
+  const currentArticles =
+    categoryArticles.length !== 0 ? categoryArticles : articles;
+
+  // CategoryData
+  const currentCategory = categories.filter((category) => {
+    return category.id === parseInt(categoryID);
+  });
+
   return (
     <div>
-      <CategoryHeader categoryName={category.name} />
+      <CategoryHeader categoryName={currentCategory[0].name} />
       <PageTemplate>
-        <ArticleList articles={articles} />
+        <ArticleList articles={currentArticles} />
       </PageTemplate>
     </div>
   );
