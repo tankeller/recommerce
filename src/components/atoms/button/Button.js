96aa998330c5254145b9f4dc64b9f96f1a3a27e3
Tan Keller <t.keller@reply.de>
2020-05-17 20:17:07 +0200

    Reworked to styled component in Button component

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -1,50 +1,61 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
+import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { useTheme } from 'emotion-theming';
 
-const Button = ({ type, size, onClick, className, disabled, children }) => {
+const Button = ({ type, size, onClick, disabled, children, ...props }) => {
+  const theme = useTheme();
+
   return (
-    <button
-      css={{
-        padding: '6px',
-        backgroundColor: 'blue',
-        color: '#fff',
-        fontWeight: 'bold',
-        borderRadius: '2px',
-        border: 'none',
-        textTransform: 'uppercase',
-        fontSize: '0.7rem',
-        cursor: 'pointer',
-
-        '&:hover': {
-          backgroundColor: 'red',
-        },
-
-        '&:disabled': {
-          backgroundColor: 'gray',
-          cursor: 'not-allowed',
-        },
-        '&.btn-small': {
-          padding: '0.5rem 0.6rem',
-        },
-        '&.btn-medium': {
-          padding: '0.8rem 1rem;',
-          fontSize: '1rem',
-        },
-        '&.btn-large': {
-          padding: '1.2rem 1.4rem',
-          fontSize: '1.33rem',
-        },
-      }}
-      className={`${className} btn btn-${size}`}
+    <StyledButton
+      theme={theme}
       onClick={onClick}
       disabled={disabled ? true : false}
       type={type}
+      {...props}
     >
       {children}
-    </button>
+    </StyledButton>
   );
 };
+
+const handleButtonSize = (size) => {
+  switch (size) {
+    case 'small':
+      return 'padding: 0.5rem 0.6rem; font-size: 0.7rem';
+    case 'medium':
+      return 'padding:0.8rem 1rem; font-size: 1rem';
+    case 'large':
+      return 'padding: 1.2rem 1.4rem; font-size: 1.33rem';
+    default:
+      return 'padding:0.5rem 0.6rem; font-size: 0.7rem';
+  }
+};
+
+export const StyledButton = styled.button`
+  background-color: #fff;
+  color: ${(props) => (props.theme ? props.theme.colors.textColor : '#61DBFB')};
+  font-weight: bold;
+  border-radius: 2px;
+  border: none;
+  text-transform: uppercase;
+  fontsize: 0.7rem;
+  cursor: pointer;
+
+  ${({ size }) => {
+    handleButtonSize(size);
+  }}
+
+  &:hover {
+    color: ${(props) =>
+      props.theme ? props.theme.colors.primaryColor : '#61DBFB'};
+  }
+
+  &:disabled {
+    background-color: gray;
+    cursor: not-allowed;
+  }
+`;
 
 Button.propTypes = {
   type: PropTypes.oneOf(['button', 'submit', 'reset']),
