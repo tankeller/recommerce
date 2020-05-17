220793b0bcd025b050b87cecb960ab013c9298f6
Tan Keller <t.keller@reply.de>
2020-05-17 20:38:32 +0200

    Fix missing size for button component

--- src/components/atoms/button/Button.js
+++ src/components/atoms/button/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { useTheme } from 'emotion-theming';
 
-const Button = ({ type, size, onClick, disabled, children, ...props }) => {
+const Button = ({ type, onClick, disabled, children, ...props }) => {
   const theme = useTheme();
 
   return (
@@ -22,37 +22,35 @@ const Button = ({ type, size, onClick, disabled, children, ...props }) => {
 const handleButtonSize = (size) => {
   switch (size) {
     case 'small':
-      return 'padding: 0.5rem 0.6rem; font-size: 0.7rem';
+      return `padding: 0.5rem 0.6rem;font-size: 0.7rem;`;
     case 'medium':
-      return 'padding:0.8rem 1rem; font-size: 1rem';
+      return `padding:0.8rem 1rem; font-size: 1rem;`;
     case 'large':
-      return 'padding: 1.2rem 1.4rem; font-size: 1.33rem';
+      return `padding: 1.2rem 1.4rem; font-size: 1.33rem;`;
     default:
-      return 'padding:0.5rem 0.6rem; font-size: 0.7rem';
+      return `padding:0.8rem 1rem; font-size: 1rem;`;
   }
 };
 
 export const StyledButton = styled.button`
   background-color: #fff;
-  color: ${(props) => (props.theme ? props.theme.colors.textColor : '#61DBFB')};
+  color: ${({ theme }) => (theme ? theme.colors.textColor : '#61DBFB')};
   font-weight: bold;
   border-radius: 2px;
   border: none;
   text-transform: uppercase;
-  fontsize: 0.7rem;
+  ${({ size }) => handleButtonSize(size)}
   cursor: pointer;
-
-  ${({ size }) => {
-    handleButtonSize(size);
-  }}
+  border: 1px solid
+    ${({ theme }) => (theme ? theme.colors.textColor : '#61DBFB')};
 
   &:hover {
-    color: ${(props) =>
-      props.theme ? props.theme.colors.primaryColor : '#61DBFB'};
+    color: ${({ theme }) => (theme ? theme.colors.primaryColor : '#61DBFB')};
   }
 
   &:disabled {
     background-color: gray;
+    color: #fff;
     cursor: not-allowed;
   }
 `;
