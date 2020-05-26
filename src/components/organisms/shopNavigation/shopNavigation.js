d42b809e596ef8fd29c5f6add95ec5e23c0cc646
Mario Meissner <m.meissner@reply.de>
2020-05-26 14:02:36 +0200

    Add appHeader styled comonents - currently broken

--- src/components/organisms/shopNavigation/shopNavigation.js
+++ src/components/organisms/shopNavigation/shopNavigation.js
@@ -1,23 +1,12 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
+import React from 'react';
 
-import Logo from '../../atoms/logo/Logo';
 import ShopActionsMenu from '../../molecules/shopActionsMenu/ShopActionsMenu';
 
 const ShopNavigation = ({ props }) => {
   return (
-    <div
-      css={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '100%',
-        minHeight: '100px',
-      }}
-      {...props}
-    >
-      <Logo />
+    <nav {...props}>
       <ShopActionsMenu />
-    </div>
+    </nav>
   );
 };
 
