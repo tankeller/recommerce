75167493f51fbae8432a9b3a0a3afba30bec4b46
Mario Meissner <m.meissner@reply.de>
2020-06-11 19:51:21 +0200

    Fix wrong button propType value in button story

--- src/components/atoms/button/button.stories.js
+++ src/components/atoms/button/button.stories.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text } from "@storybook/addon-knobs";
-
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Button from './Button';
 
 export default {
-    component: Button,
-    title: 'Atoms/Button',
-    decorators: [withKnobs]
-}
+  component: Button,
+  title: 'Atoms/Button',
+  decorators: [withKnobs],
+};
 
-export const Default = () => <Button onClick={action('onClick')}>{text("Label", "Button Text")}</Button>;
+export const Default = () => (
+  <Button onClick={action('onClick')}>{text('Label', 'Button Text')}</Button>
+);
 
-export const DefaultSmall = () => <Button size="small" onClick={action('onClick')}>{text("Label", "Button Text")}</Button>;
+export const DefaultSmall = () => (
+  <Button size="small" onClick={action('onClick')}>
+    {text('Label', 'Button Text')}
+  </Button>
+);
 
-export const DefaultMedium = () => <Button size="medium" onClick={action('onClick')}>{text("Label", "Button Text")}</Button>;
+export const DefaultMedium = () => (
+  <Button size="medium" onClick={action('onClick')}>
+    {text('Label', 'Button Text')}
+  </Button>
+);
 
-export const DefaultLarge = () => <Button size="large" onClick={action('onClick')}>{text("Label", "Button Text")}</Button>;
+export const DefaultLarge = () => (
+  <Button size="large" onClick={action('onClick')}>
+    {text('Label', 'Button Text')}
+  </Button>
+);
 
-export const Disabled = () =>
- (<Button 
-    onClick={action('onClick')}
-    disabled='true'>
-        Disabled Button
- </Button>)
+export const Disabled = () => (
+  <Button onClick={action('onClick')} disabled>
+    Disabled Button
+  </Button>
+);
