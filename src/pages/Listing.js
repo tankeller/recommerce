a21bb017788f1effe5e96f5a0e8796f09b3271d7
Tan Keller <t.keller@reply.de>
2020-06-16 17:04:03 +0200

    Get product listing data from API if possible

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -6,25 +6,54 @@ import SectionWrapper from '../components/atoms/sectionWrapper/SectionWrapper';
 import ArticleList from '../components/organisms/articleList/ArticleList';
 import CategoryHeader from '../components/organisms/categoryHeader/CategoryHeader';
 
+import useDataFetching from '../assets/hooks/useDataFetching';
 import articles from '../assets/static/articles.json';
-import categories from '../assets/static/categories.json';
+import mockCategories from '../assets/static/categories.json';
 
 const Listing = ({ categoryID }) => {
-  // Articles by categoryID
-  const categoryArticles = articles.filter((article) => {
-    return article.categoryID === parseInt(categoryID);
-  });
+  let currentArticles = [];
 
-  // Articles Fallback: All articles
-  const currentArticles =
-    categoryArticles.length !== 0 ? categoryArticles : articles;
+  const { loading, results, error } = useDataFetching(
+    `/store-api/v1/product-listing/${categoryID}`,
+    {
+      includes: {
+        product_listing_sorting: ['key'],
+        product_manufacturer: ['id', 'name'],
+        price_aggregation: ['min', 'max'],
+        product_group: ['id', 'name', 'options'],
+        product_group_option: ['id', 'name'],
+        product: ['id', 'name', 'calculatedPrice', 'cover'],
+        media: ['id', 'url'],
+        product_media: ['media'],
+        calculated_price: ['unitPrice', 'totalPrice'],
+      },
+    }
+  );
+
+  if (loading) {
+    return <p>...Loading Articles</p>;
+  } else if (error) {
+    // Articles by categoryID
+    const categoryArticles = articles.filter((article) => {
+      return article.categoryID === parseInt(categoryID);
+    });
+
+    // Articles Fallback: All articles
+    currentArticles =
+      categoryArticles.length !== 0 ? categoryArticles : articles;
+  } else if (results) {
+    currentArticles = results.elements;
+  }
 
   // CategoryData
-  const currentCategory = categories.filter((category) => {
+  const currentCategory = mockCategories.filter((category) => {
     return category.id === parseInt(categoryID);
   });
 
   // BoxLayout Fallback
+  if (currentCategory.length === 0) {
+    currentCategory[0] = mockCategories[0];
+  }
   const boxLayout = !currentCategory[0].layout
     ? 'basic'
     : currentCategory[0].layout;
