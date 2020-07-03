7541711f7b7ea3d7a0511c7ca5f2d400082e3bab
Tan Keller <t.keller@reply.de>
2020-07-03 15:03:01 +0200

    Return mockArticle if no data given

--- src/assets/utils/api/shopwareArticleProcessor.js
+++ src/assets/utils/api/shopwareArticleProcessor.js
@@ -1,23 +1,34 @@
+import mockArticles from '../../static/articles_sw.json';
+
 function showGrossPrices() {
   return true;
 }
 
+export const processArticle = (article) => {
+  if (!article) {
+    article = mockArticles[0];
+    return article;
+  }
+
+  return {
+    active: article.active,
+    id: article.id,
+    img: article.cover.media.url,
+    name: article.name,
+    supplier: 'Marvel',
+    categoryID: article.categoryTree.slice(-1, 0),
+    price: showGrossPrices() ? article.price[0].gross : article.price[0].net,
+    listPrice: article.price.listPrice ? article.price.listPrice : null,
+    isNew: article.isNew,
+    stock: article.stock,
+    description: article.description,
+    closeout: article.isCloseout,
+  };
+};
+
 export default function processShopwareArticleData(shopwareArticles) {
   const processedArticles = shopwareArticles.map((article) => {
-    return {
-      active: article.active,
-      id: article.id,
-      img: article.cover.media.url,
-      name: article.name,
-      supplier: 'Marvel',
-      categoryID: article.categoryTree.slice(-1, 0),
-      price: showGrossPrices() ? article.price[0].gross : article.price[0].net,
-      listPrice: article.price.listPrice ? article.price.listPrice : null,
-      isNew: article.isNew,
-      stock: article.stock,
-      description: article.description,
-      closeout: article.isCloseout,
-    };
+    return processArticle(article);
   });
 
   return processedArticles;
